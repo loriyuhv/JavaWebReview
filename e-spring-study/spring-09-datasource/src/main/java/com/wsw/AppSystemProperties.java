aba57c6f8bdf510c378845527777b043bcd1cf37
loriyuhv <2385923602@qq.com>
2025-08-26 17:39:41 +0800

    feat(ioc-xml): 实现XML配置的DataSource类型注入

---
+++ e-spring-study/spring-09-datasource/src/main/java/com/wsw/AppSystemProperties.java
@@ -0,0 +1,18 @@
+package com.wsw;
+
+import com.wsw.dao.BookDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author loriyuhv
+ * @date 2025/8/13
+ * @description 加载Properties文件运行
+ */
+public class AppSystemProperties {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookDao bookDao = (BookDao) context.getBean("bookDao");
+        bookDao.save();
+    }
+}
