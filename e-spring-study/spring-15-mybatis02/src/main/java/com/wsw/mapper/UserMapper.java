f77c2225eb080a4ac3f354480dfc50675bd79bfa
loriyuhv <2385923602@qq.com>
2025-08-27 22:00:55 +0800

    feat(mybatis02): 实现MyBatisMapper代理开发示例

---
+++ e-spring-study/spring-15-mybatis02/src/main/java/com/wsw/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.wsw.mapper;
+
+import com.wsw.domain.User;
+
+/**
+ * @author loriyuhv
+ * @date 2025/8/27 21:09
+ * @description
+ */
+public interface UserMapper {
+    int insertUser(User user);
+}
