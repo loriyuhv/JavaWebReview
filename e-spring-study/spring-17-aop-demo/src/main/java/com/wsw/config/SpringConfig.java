a091334224b4c0a751918463b265f96fd21979eb
loriyuhv <2385923602@qq.com>
2025-08-28 15:54:27 +0800

    feat(aop-demo):实现Spring Aop demo(需完善）

---
+++ e-spring-study/spring-17-aop-demo/src/main/java/com/wsw/config/SpringConfig.java
@@ -0,0 +1,14 @@
+package com.wsw.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author loriyuhv
+ * @date 2025/8/28 13:02
+ * @description
+ */
+@Configuration
+@ComponentScan("com.wsw")
+public class SpringConfig {
+}
---
+++ e-spring-study/spring-17-aop-demo/src/main/java/com/wsw/dao/BookDao.java
@@ -0,0 +1,13 @@
+package com.wsw.dao;
+
+/**
+ * @author loriyuhv
+ * @date 2025/8/28 12:54
+ * @description
+ */
+public interface BookDao {
+    void save();
+    void update();
+    void delete();
+    void select();
+}
