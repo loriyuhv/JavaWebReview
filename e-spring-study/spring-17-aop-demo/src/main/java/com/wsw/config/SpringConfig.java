ec7da4f548288655d87ae98715ab2a3faa2fe1a3
loriyuhv <2385923602@qq.com>
2025-08-28 19:32:57 +0800

    feat: 完善AOP demo模块

--- e-spring-study/spring-17-aop-demo/src/main/java/com/wsw/config/SpringConfig.java
+++ e-spring-study/spring-17-aop-demo/src/main/java/com/wsw/config/SpringConfig.java
@@ -2,6 +2,7 @@ package com.wsw.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @author loriyuhv
@@ -10,5 +11,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ComponentScan("com.wsw")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class SpringConfig {
 }
