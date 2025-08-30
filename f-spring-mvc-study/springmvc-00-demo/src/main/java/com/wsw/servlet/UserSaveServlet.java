cbaa0a96d4dd99cbcd85902722b240edfe583321
loriyuhv <2385923602@qq.com>
2025-08-30 08:58:12 +0800

    feat(springmvc-demo):添加SpringMVC与HttpServlet处理对比示例

---
+++ f-spring-mvc-study/springmvc-00-demo/src/main/java/com/wsw/servlet/UserSaveServlet.java
@@ -0,0 +1,33 @@
+package com.wsw.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author loriyuhv
+ * @date 2025/8/30 8:44
+ * @description
+ */
+@WebServlet("/user/save")
+public class UserSaveServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        // 1. 接受请求参数
+        String name = req.getParameter("name");
+        System.out.println("servlet save name ===> " + name);
+        // 2. 产生响应
+        resp.setContentType("text/json;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.write("{'module': 'servlet save'}");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        this.doGet(req, resp);
+    }
+}
