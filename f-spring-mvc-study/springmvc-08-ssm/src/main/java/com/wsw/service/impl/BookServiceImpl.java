298cf015a19cdaa2bd0d9d3bb3a7ba4814397b42
loriyuhv <2385923602@qq.com>
2025-09-01 06:43:43 +0800

    chore(springmvc-ssm):事务管理功能优化

--- f-spring-mvc-study/springmvc-08-ssm/src/main/java/com/wsw/service/impl/BookServiceImpl.java
+++ f-spring-mvc-study/springmvc-08-ssm/src/main/java/com/wsw/service/impl/BookServiceImpl.java
@@ -4,6 +4,7 @@ import com.wsw.dao.BookDao;
 import com.wsw.domain.Book;
 import com.wsw.service.BookService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
  * @description
  */
 @Service
+@Transactional
 public class BookServiceImpl implements BookService {
     private final BookDao bookDao;
 
