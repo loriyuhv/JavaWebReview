73f536e7b5cee55dc11d88024cd5985426cde853
loriyuhv <2385923602@qq.com>
2026-03-25 15:38:59 +0800

    chore(spring-study):Bean生命周期复习

--- e-spring-study/spring-04-bean-lifecycle/src/main/java/com/wsw/AppForLifeCycle.java
+++ e-spring-study/spring-04-bean-lifecycle/src/main/java/com/wsw/AppForLifeCycle.java
@@ -1,25 +1,24 @@
 package com.wsw;
 
-import com.wsw.dao.BookDao;
 import com.wsw.service.BookService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
+ * Bean的生命周期
+ *
  * @author loriyuhv
- * @date 2025/8/26 14:27
- * @description Bean的生命周期
+ * @version 1.0 2025/8/26 14:27
  */
 public class AppForLifeCycle {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
         // 设置容器注册关闭钩子。虚拟机在关闭前先关闭容器
         context.registerShutdownHook();
-        BookDao bookDao = (BookDao) context.getBean("bookDao");
-        bookDao.save();
-        // 这种方式比较暴力
-        // context.close();
 
         BookService bookService = (BookService) context.getBean("bookService");
         bookService.save();
+
+        // 这种方式比较暴力
+        // context.close();
     }
 }
