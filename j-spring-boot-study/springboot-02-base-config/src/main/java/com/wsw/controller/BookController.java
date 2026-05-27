b9f66df2f0bd56c55c8763f484a8749f85980850
loriyuhv <2385923602@qq.com>
2026-05-27 17:21:31 +0800

    refactor(j-spring-boot-study): 重构了01和02的代码，让其更合理

--- j-spring-boot-study/springboot-02-base-config/src/main/java/com/wsw/controller/BookController.java
+++ j-spring-boot-study/springboot-02-base-config/src/main/java/com/wsw/controller/BookController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author loriyuhv
- * @date 2025/9/2 9:49
- * @description
+ * @version 1.0 2025/9/2 9:49
  */
 @RestController
 @RequestMapping("/books")
